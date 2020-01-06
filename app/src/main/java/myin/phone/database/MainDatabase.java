121f3262ea730859e540796cd2dd8cede05c302a
myin <illuslion1998@gmail.com>
2020-01-06 10:38:46 +0100

    Fix build warning

--- app/src/main/java/myin/phone/database/MainDatabase.java
+++ app/src/main/java/myin/phone/database/MainDatabase.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 import myin.phone.data.app.HomeApp;
 import myin.phone.data.app.HomeAppRepository;
 
-@Database(entities = {HomeApp.class}, version = 1)
+@Database(entities = {HomeApp.class}, version = 1, exportSchema = false)
 public abstract class MainDatabase extends RoomDatabase {
     abstract HomeAppRepository homeAppRepository();
 }
