f87e4f11418c5abc5b11bc2bedd0e70dbddcefc8
myin <minyin142p@gmail.com>
2020-05-02 09:42:04 +0200

    Revert to old HomeBottom

--- app/src/main/java/io/github/myin/phone/views/home/HomeBottom.java
+++ app/src/main/java/io/github/myin/phone/views/home/HomeBottom.java
@@ -5,56 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import dagger.android.AndroidInjection;
 import io.github.myin.phone.R;
-import io.github.myin.phone.data.BaseAppDiffCallback;
-import io.github.myin.phone.data.tool.HomeTool;
-import io.github.myin.phone.data.tool.HomeToolRepository;
-import io.github.myin.phone.list.NoScrollLinearLayout;
-import io.github.myin.phone.list.TextListAdapter;
-
-import javax.inject.Inject;
+import io.github.myin.phone.views.settings.Settings;
 
 public class HomeBottom extends Fragment {
 
-    private TextListAdapter<HomeTool> toolAdapter;
-
-    @Inject
-    HomeToolRepository homeToolRepository;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-//        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
-
-        toolAdapter = new TextListAdapter<>(new BaseAppDiffCallback<>());
-        toolAdapter.setOnItemClickListener(homeTool -> {
-            Intent appIntent = homeTool.getActivityIntent();
-            startActivity(appIntent);
-        });
-
-        homeToolRepository.getHomeToolSorted().observe(this, appList -> {
-            toolAdapter.submitList(appList);
-        });
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.home_bottom, container, false);
-
-        if (view != null) {
-            RecyclerView appsView = view.findViewById(R.id.apps_list);
-            appsView.setLayoutManager(new NoScrollLinearLayout(getContext(), LinearLayoutManager.HORIZONTAL));
-            appsView.setAdapter(toolAdapter);
-        }
-
-        return view;
+        return inflater.inflate(R.layout.home_bottom, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView settingsIcon = view.findViewById(R.id.settings);
+        settingsIcon.setOnClickListener((View v) -> openSettings());
+    }
+
+    public void openSettings() {
+        Intent appsIntent = new Intent(getContext(), Settings.class);
+        getActivity().startActivityForResult(appsIntent, HomeActivity.REQ_APPS_CHANGED);
+    }
 }
